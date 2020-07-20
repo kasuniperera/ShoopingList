d632e7d9a060f5afbab30a149969bb27b2ee8503
kasuniperera <kasunilakshika96@gmail.com>
2020-07-20 12:14:13 +0530

    app.js content move to home.js file

--- App.js
+++ App.js
@@ -7,141 +7,28 @@ import TodoList from './components/TodoList';
 import AddListModal from './components/AddListModal';
 import Fire from './Fire';
 import { firestore } from 'firebase';
+import Home from './components/Home';
+import { createAppContainer,createSwitchNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
 
-  state = {
-    addTodoVisible : false,
-    lists:[],
-    user:{},
-    loading:true
-  }
-
-  componentDidMount(){
-    firebase = new Fire((error,user) => {
-      if(error){
-        return alert("something went wrong");
-      }
-      firebase.getLists(lists => {
-        this.setState({lists,user}, () => {
-          this.setState({loading: false});
-        });
-      });
-      this.setState({user});
-    });
+  const AppSwitchNavigator = createSwitchNavigator({
+    Home:Home
+  });
+  
+  const AppNavigator = createAppContainer(AppSwitchNavigator);
+  // name="Signup" 
+  
+  export default class App extends React.Component {
+    render (){
+      return <AppNavigator/>;
+    }
   }
   
-  //  componentWillMount(){
-  //    firebase.detach();
-  //  }
-
-
-  toggleAddTodoModal(){
-    this.setState({addTodoVisible: !this.state.addTodoVisible})
-  }
-
-  renderList = list => {
-    return <TodoList list={list} updateList={this.updateList}/>;
-  }
-
-  addList = list => {
-    firebase.addList({
-      name: list.name,
-      color:list.color,
-      todos:[]
-    });
-  };
-
-  updateList = list => {
-    firebase.updateList(list);
-  };
-
-  render(){
-    if(this.state.loading){
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator size="large" color={colors.blue}/>
-        </View>
-      )
-    }
-    return (
-      <View style={styles.container}>
-        <Modal 
-          animationType="slide" 
-          visible={this.state.addTodoVisible} 
-          onRequestClose={() => this.toggleAddTodoModal()}
-        >
-            <AddListModal closeModal={() => this.toggleAddTodoModal()} addList={this.addList}/>
-        </Modal>
-        <View>
-          <Text>User:{this.state.user.uid}</Text>
-        </View>
-
-        <View style={{flexDirection:"row"}}>
-          <View style={styles.divider} />
-          <Text style={styles.title}>
-            Grocery <Text style={{fontWeight:"250" , color:colors.blue,}}>Lists</Text>
-          </Text>
-          <View style={styles.divider} />
-        </View>
-
-        <View style={{marginVertical:48}}>
-          <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
-            <AntDesign name="plus" size={16} color={colors.blue}/>
-          </TouchableOpacity>
-          <Text style={styles.add}>Add List</Text>
-        </View>
-
-        <View style={{height: 350, padding:32}}>
-          <FlatList 
-            data={this.state.lists} 
-            keyExtractor={item => item.id.toString() } 
-            horizontal={true} 
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item}) => (
-              this.renderList(item)
-            )}
-            keyboardShouldPersistTaps="always"
-          />
-          
-        </View>
-      </View>
-    );
-  }
- 
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  divider:{
-    backgroundColor: colors.lightBlue,
-    height:1,
-    flex:1,
-    alignSelf: "center"
-  },
-  title:{
-    fontSize:38,
-    fontWeight: "800",
-    color: colors.black,
-    paddingHorizontal:64
-  },
-  addList:{
-    borderWidth:2,
-    borderColor: colors.lightBlue,
-    borderRadius: 4,
-    padding: 16,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  add:{
-    color:colors.blue,
-    fontWeight: "600",
-    fontSize: 14,
-    marginTop: 8
-  }
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
